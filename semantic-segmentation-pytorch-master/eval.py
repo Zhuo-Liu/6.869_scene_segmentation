484a1a9e3aea9ec5f25c66f6dd1253564e54a4a8
uniqueyangfan <yangf@mit.edu>
2022-04-16 11:39:02 -0400

    file path of color150

--- semantic-segmentation-pytorch-master/eval.py
+++ semantic-segmentation-pytorch-master/eval.py
@@ -18,7 +18,7 @@ from mit_semseg.lib.utils import as_numpy
 from PIL import Image
 from tqdm import tqdm
 
-colors = loadmat('data/color150.mat')['colors']
+colors = loadmat('./drive/MyDrive/scene-segmentation/data/color150.mat')['colors']
 
 
 def visualize_result(data, pred, dir_result):
